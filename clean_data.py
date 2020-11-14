ff144c41d0edb78f2136114dccf4e453d8d83961
lerakravchik <kra.valeriia@campus.technion.ac.il>
2020-11-14 14:59:56 +0200

    We did exersise number 1. we are happy.

--- clean_data.py
+++ clean_data.py
@@ -6,7 +6,7 @@ Created on Sun Jul 21 17:14:23 2019
 """
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 
 def rm_ext_and_nan(CTG_features, extra_feature):
@@ -17,8 +17,14 @@ def rm_ext_and_nan(CTG_features, extra_feature):
     :return: A dictionary of clean CTG called c_ctg
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+    # c_ctg = CTG_features.copy()
+    # c_ctg.drop(extra_feature,axis=1).dropna(how='all')
     c_ctg = CTG_features.copy()
-    c_ctg.drop(extra_feature,axis=1).dropna(how='all')
+    c_ctg = c_ctg.drop(extra_feature, axis=1)
+    c_ctg.loc[:, 'LB':'Tendency'] = c_ctg.loc[:, 'LB':'Tendency'].replace('--', np.nan).replace('#', np.nan).replace(
+        'Nan', np.nan)
+    c_ctg = c_ctg.dropna()
+
     # --------------------------------------------------------------------------
     return c_ctg
 
@@ -33,8 +39,17 @@ def nan2num_samp(CTG_features, extra_feature):
     c_cdf = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
     c_cdf = CTG_features.copy()
-    c_cdf.drop(extra_feature).mask(CTG_features.isnull(), np.random.choice([CTG_features]))
+    c_cdf = c_cdf.drop(extra_feature, axis=1)
+    c_cdf.loc[:, 'LB':'Tendency'] = c_cdf.loc[:, 'LB':'Tendency'].replace('--', np.nan).replace('#', np.nan).replace(
+        'Nan', np.nan)
+    for i in c_cdf.keys():
+        for j in range(1, len(c_cdf)):
+            if np.isnan(c_cdf.loc[j, i]):
+                c_cdf.loc[j, i] = np.random.choice(c_cdf.loc[:, i].to_list())
+
+
     # -------------------------------------------------------------------------
+
     return pd.DataFrame(c_cdf)
 
 
@@ -45,7 +60,17 @@ def sum_stat(c_feat):
     :return: Summary statistics as a dicionary of dictionaries (called d_summary) as explained in the notebook
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    c = c_feat.describe().to_dict()
+    for i in c.keys():
+        #     print(c[i])
+        c[i].pop('mean')
+        c[i].pop('std')
+        c[i].pop('count')
+        c[i]['Q1'] = c[i].pop('25%')
+        c[i]['median'] = c[i].pop('50%')
+        c[i]['Q3'] = c[i].pop('75%')
+        c[i]['max'] = c[i].pop('max')
+    d_summary = c
     # -------------------------------------------------------------------------
     return d_summary
 
@@ -59,7 +84,18 @@ def rm_outlier(c_feat, d_summary):
     """
     c_no_outlier = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    c = c_feat.copy()
+    IQR = {}
+    low_bound = {}
+    high_bound = {}
+    for i in c.keys():
+        IQR[i] = d_summary[i]['Q3'] - d_summary[i]['Q1']
+        low_bound[i] = d_summary[i]['Q1'] - 1.5 * IQR[i]
+        high_bound[i] = d_summary[i]['Q3'] + 1.5 * IQR[i]
+        for j in range(1, len(c_feat)):
+            if c.loc[j, i] < low_bound[i] or c.loc[j, i] > high_bound[i]:
+                c.loc[j, i] = np.nan
+    c_no_outlier = c
     # -------------------------------------------------------------------------
     return pd.DataFrame(c_no_outlier)
 
@@ -73,7 +109,11 @@ def phys_prior(c_cdf, feature, thresh):
     :return: An array of the "filtered" feature called filt_feature
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:-----------------------------
-
+    filt_feature = c_cdf.copy()
+    for i in range(1, len(filt_feature)):
+        if filt_feature[feature][i] > thresh:
+            filt_feature[feature][i] = np.nan
+    filt_feature.dropna()
     # -------------------------------------------------------------------------
     return filt_feature
 
@@ -89,6 +129,24 @@ def norm_standard(CTG_features, selected_feat=('LB', 'ASTV'), mode='none', flag=
     """
     x, y = selected_feat
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    c = CTG_features.describe().to_dict()
+    cc = CTG_features.copy()
+    if mode == 'standard':
+        for i in cc.keys():
+            cc[i] = (cc[i] - c[i]['mean'])/c[i]['std']
+    if mode == 'MinMax':
+        for i in cc.keys():
+            cc[i] = (cc[i] - c[i]['min'])/(c[i]['max']-c[i]['min'])
+    if mode == 'mean':
+        for i in cc.keys():
+            cc[i] = (cc[i] - c[i]['mean'])/(c[i]['max']-c[i]['min'])
+    xlbl = ['beats/min','%']
+    t = [x,y]
+    axarr = cc.hist(column=[x,y], bins=100,layout = (2, 1),figsize=(20, 10))
+    for i,ax in enumerate(axarr.flatten()):
+        ax.set_xlabel(xlbl[i])
+        ax.set_ylabel('count')
+        ax.set_title(t[i])
+    nsd_res = cc
     # -------------------------------------------------------------------------
     return pd.DataFrame(nsd_res)
